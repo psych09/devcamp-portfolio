00aca13738fd30a81eb0cb0c083037b9f3f9372d
Jordan Hudgens <jed.flynn.09@gmail.com>
2018-03-07 17:27:52 +0000

    Updated default page title

--- app/controllers/concerns/default_page_content.rb
+++ app/controllers/concerns/default_page_content.rb
@@ -6,7 +6,7 @@ module DefaultPageContent
   end
   
   def set_page_defaults
-    @page_title = "DevcampPortfolio | My Portfolio Website"
-    @seo_keywords = "Jed Flynn portfolio Ruby web development"
+    @page_title = "Jed Flynn | Portfolio website"
+    @seo_keywords = "Jed Flynn portfolio Ruby Angular web development"
   end
 end
